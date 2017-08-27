9aeb0db56174c2833612700bd26cb7cb9d6572aa
zymap <734206637@qq.com>
2017-08-27 15:00:16 +0800

    test1

---
+++ test/util/Onephase.java
@@ -0,0 +1,16 @@
+package util;
+
+/**
+ * Created by zy on 17-8-27.
+ */
+public class Onephase {
+    public static void main(String[] args) {
+        com.company.util.response.Onephase onephase = new com.company.util.response.Onephase();
+        String xxx = "680136321100006803083235b43a343333333334b935333333383334483633333373557334c733383433bf16";
+        String sss = onephase.split(xxx);
+        System.out.println(sss);
+    }
+}
+//3334b935333333383334483633333373557334c733383433
+//3334b935333333383334483633333373557334c733383433
+//333333383334483633333373557334c733383433
